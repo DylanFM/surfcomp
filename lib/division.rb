9683cfbcd907ae74d1b1b5bef1477d9e2396213c
Dylan Fogarty-MacDonald <dylan.fm@gmail.com>
2008-08-24 00:12:58 +1000

    Heats formed and tested

--- lib/division.rb
+++ lib/division.rb
@@ -1,12 +1,26 @@
 class Division
   
-  attr_accessor :name, :competitors
+  attr_reader :name, :competitors, :heats
   
   def initialize(name, competitors)
     @name = name
     @competitors = competitors
+    create_heats()
   end
   
-  
+  def create_heats
+    #Heats contain 3-5 competitors, preferably 4
+    @heats = []
+    @heat = []
+    @active_competitors = []
+    @competitors.each do |competitor|
+      @active_competitors << competitor
+      @heat << competitor
+      if @heat.length == 4 || @competitors.length - @active_competitors.length < 4
+        @heats << @heat
+        @heat = []
+      end
+    end
+  end
   
 end
--- test/test_division.rb
+++ test/test_division.rb
@@ -6,13 +6,22 @@ class TestDivision < Test::Unit::TestCase
   
   def setup
     @name = 'Opens'
-    @competitors = ['Dylan Fogarty-MacDonald', 'Luke Kannar', 'Kelly Slater', 'Matt Graham-Ellison']
+    @competitors = ['Dylan Fogarty-MacDonald', 'Luke Kannar', 'Kelly Slater', 'Matt Graham-Ellison', 'Tom Carroll', 'Barton Lynch', 'Kent MacDonald', 'David MacDonald', 'Manoa Drollet', 'Shane Beschen', 'Luke Egan', 'Mark Occilupo', 'Shane Powell', 'Shaun Oldfield', 'Dave Oldfield', 'Ulrich Jaeger', 'Belynda Jaeger']
     @division = Division.new(@name, @competitors)
   end
   
   def test_new_division
     assert_equal(@name, @division.name)
     assert_equal(@competitors, @division.competitors)
+    assert_equal(false, @division.heats.empty?)
+  end
+  
+  def test_all_competitors_in_heats
+    @active_competitors = []
+    @division.heats.each do |heat|
+      heat.each { |competitor| @active_competitors << competitor }
+    end
+    assert_equal(@competitors, @active_competitors)
   end
   
 end
