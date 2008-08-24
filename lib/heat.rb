24c3fc8c100f526918172a2098e4bed5447c13eb
Dylan Fogarty-MacDonald <dylan.fm@gmail.com>
2008-08-24 22:26:55 +1000

    Added details to the heat class and changed around the heat test

--- lib/heat.rb
+++ lib/heat.rb
@@ -1,6 +1,7 @@
 class Heat
   
   attr_reader :competitors
+  attr_accessor :location, :time, :length
   
   def initialize(competitors = [])
     @competitors = competitors
@@ -8,6 +9,12 @@ class Heat
   
   def add_competitor(competitor)
     @competitors << competitor
+  end
+  
+  def details
+    output = "The heat starts at #@time and goes for #@length minutes. It takes place at #@location and consists of the following competitors:"
+    @competitors.each { |competitor| output += "\n\t- #{competitor}" }
+    output
   end
   
 end
--- test/tc_heat.rb
+++ test/tc_heat.rb
@@ -5,23 +5,36 @@ require 'Heat'
 class TestHeat < Test::Unit::TestCase
   
   def setup
+    #Heat info
     @competitors = ['Dylan Fogarty-MacDonald', 'Luke Kannar', 'Kelly Slater', 'Matt Graham-Ellison', 'Tom Carroll']
-    @heat = Heat.new
+    @time = '10:30am'
+    @location = 'North bank'
+    @length = '25'
+    #Make heat
+    @heat = Heat.new(@competitors)
+    @heat.time = @time
+    @heat.location = @location
+    @heat.length = @length
   end
-  
-  def test_competitors_are_absent
-    assert(@heat.competitors.empty?, 'Heat should not have competitors')
+
+  def test_competitors_added_at_init
+    assert_equal(@competitors, @heat.competitors)
   end
   
   def test_competitor_is_added
-    competitor = @competitors.pop
+    competitor = 'Marshall McLuhan'
     @heat.add_competitor(competitor)
     assert_equal(competitor, @heat.competitors.last)
   end
   
-  def test_competitors_added_at_init
-    @heat = Heat.new(@competitors)
-    assert_equal(@competitors, @heat.competitors)
+  def test_heat_details_include_all_info
+    #Details should include time, location, all competitiors and the heat length
+    assert(@heat.details =~ Regexp.new(@time), 'Heat details should include time')
+    assert(@heat.details =~ Regexp.new(@location), 'Heat details should include location')
+    assert(@heat.details =~ Regexp.new(@length), 'Heat details should include length')
+    @competitors.each { |c| 
+      assert(@heat.details =~ Regexp.new(c), "Heat details should include competitor: #{c}")
+    }
   end
   
 end
