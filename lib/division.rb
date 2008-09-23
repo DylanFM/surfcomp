b75335af4c8f98b74ee07c74d453c4a05a7d9e83
Dylan Fogarty-MacDonald <dylan.fm@gmail.com>
2008-09-23 22:05:16 +1000

    Adjusted division.create_heats. Removed code making heats proper sizes. I think I'll write a competition director class to manage heats which will check out heats that have been built and make sure they're OK. The whole heat building this should be there anyway (divisions don't build their own heats, an overall comp director does).

--- lib/division.rb
+++ lib/division.rb
@@ -5,29 +5,18 @@ class Division
   BEST_HEAT_SIZE = 4
   
   def initialize(name, competitors)
-    @name = name
+    @name, @heats = name, []
     @competitors = competitors.collect! { |c| Competitor.new(c) }
     create_heats
   end
   
   def create_heats
-    @heats, active = [], []
-    # => I want a heat to have 4 competitors, but if we're going to end up with a 1-3 sized heat at the end I want to have a couple of 5 person heats to avoid this.
-    heats_count = @competitors.length.quo(BEST_HEAT_SIZE)
-    extra_competitors = Integer((heats_count - heats_count.to_i) * BEST_HEAT_SIZE)
-    #Start building heats
-    heat, extra = Heat.new, false
+    heat = Heat.new
     @competitors.each do |competitor|
       heat.add_competitor(competitor)
-      active << competitor
-      if heat.competitors.length == BEST_HEAT_SIZE || extra 
-        if extra_competitors == 0 || extra
-          @heats << heat
-          heat, extra = Heat.new, false
-        else
-          extra_competitors -= 1
-          extra = true
-        end
+      if heat.competitors.length == BEST_HEAT_SIZE or competitor == @competitors.last
+        @heats << heat
+        heat = Heat.new
       end
     end
   end
