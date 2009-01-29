e5c2179a10ed6f4ab8718eec8447050039c65438
DylanFM <dylan.fm@gmail.com>
2009-01-30 00:16:33 +1100

    Saving old scores when a new heat starts. Not too sure why.

--- lib/judge.rb
+++ lib/judge.rb
@@ -1,12 +1,13 @@
 class Judge
   
-  attr_reader :name, :scores
+  attr_reader :name, :scores, :old_scores
   
   def initialize(name)
-    @name, @scores = name, {}
+    @name, @scores, @old_scores = name, {}, []
   end
   
   def new_heat(competitors)
+    old_scores << scores.dup
     competitors.each do |c|
       scores[c.object_id.to_sym] = []
     end
